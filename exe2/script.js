8969db1857a543ef6e49f659a13029b4e59e8176
luizcastilho2ds <luizota2009@gmail.com>
2025-05-14 08:05:07 -0300

    Add files via upload

--- exe2/script.js
+++ exe2/script.js
@@ -5,12 +5,12 @@ function calculo() {
 
     anoNascimento = Number(window.prompt('digite o seu ano de nascimento'))
     idade = 2025 - anoNascimento
-    alert("Sua idade é")
+    alert("Sua idade é" + idade)
 
     if (idade > 18) {
         alert("Você pode tirar carteira de motorista")
     } else {
         let dif = 18 - idade
-        alert("Você não pode tirar carteira de motorista pois ainda faltam" + dif + "anos para completar 18 anos")
+        alert("Você não pode tirar carteira de motorista pois ainda faltam " + dif + " anos para completar 18 anos")
     }
 }
