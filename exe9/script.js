99391aaf91926484a9508340b7104cad8253ec94
luizcastilho2ds <luizota2009@gmail.com>
2025-06-23 11:47:32 -0300

    Add files via upload

---
+++ exe9/script.js
@@ -0,0 +1,12 @@
+let n1;
+let n2;
+
+function diferenca(){
+    n1 = window.prompt("digite o primeiro número")
+    n2 = window.prompt("digite o segundo número")
+    if (n1 > n2){
+        alert(" o primeiro número é maior");
+    } else {
+        alert(" o segundo número é maior");
+    }
+}
